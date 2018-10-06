74852d69894140ff6d5d17cc8adeafbbf457f23d
sanae lotfi <Lotfi-sanaa@hotmail.fr>
2018-10-06 14:50:22 -0400

    djikstra

--- bot/test.py
+++ bot/test.py
@@ -1,45 +1,78 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Oct  6 12:32:48 2018
-
-@author: Sanae LOTFI
-"""
+from helper import *
+from .phase_1 import move_home
 
 from helper import *
 
+def get_weights(end_position,playerInfo, gameMap):
+    edges = []
+    start_node = (playerInfo.Position.x,playerInfo.Position.y)
+    end_node= (end_position.x,end_position.y)
+    # creating edges horizotally
+    range_x = abs(end_node[0] - start_node[0])
+    range_y = abs(end_node[1] - start_node[1])
+    min_x = (end_node[0], start_node[0])
+    min_y = (end_node[1], start_node[1])
+    for j in range(range_y):
+        for i in range(range_x):
+            if gameMap.getTileAt(Point(min_x+i+1,min_y+j)) == TileContent.Empty:
+                edges.append(((min_x+i,min_y+j),(min_x+i+1,min_y+j)))
+                edges.append(((min_x+i+1,min_y+j),(min_x+i,min_y+j)))
+    # creating edges vertically
+    for i in range(range_x):
+        for j in range(range_y):
+            if gameMap.getTileAt(Point(min_x+i,min_y+j+1)) == TileContent.Empty:
+                edges.append(((min_x+i,min_y+j),(min_x+i,min_y+j+1)))
+                edges.append(((min_x+i,min_y+j+1),(min_x+i,min_y+j)))                
+    return edges
 
-class Bot:
-    def __init__(self):
-        pass
 
-    def before_turn(self, playerInfo):
-        """
-        Gets called before ExecuteTurn. This is where you get your bot's state.
-            :param playerInfo: Your bot's current state.
-        """
-        self.PlayerInfo = playerInfo
 
-    def execute_turn(self, gameMap, visiblePlayers):
-        """
-        This is where you decide what action to take.
-            :param gameMap: The gamemap.
-            :param visiblePlayers:  The list of visible players.
-        """
-        mapped_zone = {}
-        current_position = self.Position
-        explore = True
-        next_position = get_random_direction()
-        value_position = getTileAt(self, next_position)
-        mapped_zone[next_position] = value_position
-        if value_position == 4:
-            create_collect_action(next_position)
-            
-            
-        # Write your bot here. Use functions from aiHelper to instantiate your actions.
-        return create_move_action(Point(1, 0))
+def fastest_path_estimation(end_position,playerInfo,edges):
+    """
+    Returns the time spent on the fastest path between 
+    a given node "start" and all other nodes
+    All_nodes must contain "start" also
+    """
+    end_node = (end_position.x,end_position.y)
+    start_node = (playerInfo.Position.x,playerInfo.Position.y)
+    nodes = set()
+    for edge in edges:
+        nodes.add(edge[0])
+        nodes.add(edge[1])
+    all_nodes = list(nodes)
+    lst_nodes = list(set(all_nodes) - set([start_node]))
+    # nodes represents the minimal distance - at a given iteration - of each node to c
+    nodes = {x:float("inf") for x in lst_nodes}
+    nodes[start_node] = 0
+    nodes_out = []
+    min_dis_node = start_node
+    while min_dis_node != end_node:
+        next_nodes = [(min_dis_node[0]+1,min_dis_node[1]),
+                      (min_dis_node[0]-1,min_dis_node[1]),
+                      (min_dis_node[0],min_dis_node[1]+1),
+                      (min_dis_node[0],min_dis_node[1]-1)]
+        for node in next_nodes:
+            value = nodes[node]
+            if node not in nodes_out:
+                nodes[node] = min(value, nodes[min_dis_node]+1)
+        nodes_out.append(min_dis_node)
+        if len(nodes)!= len(nodes_out):
+            min_dis_node = min({k : nodes[k] for k in set(nodes) - set(nodes_out) }, key=nodes.get)
 
-    def after_turn(self):
-        """
-        Gets called after executeTurn
-        """
-        pass
+    return nodes[end_node]
+
+def yolo_swag_phase_2(playerInfo, gameMap):
+    if playerInfo.CarriedResources == playerInfo.CarryingCapacity:
+        return create_move_action(DOWN)
+    else:
+        if gameMap.getTileAt(playerInfo.Position + UP) == TileContent.Wall:
+            print('Wall at up')
+            return create_attack_action(UP)
+        if gameMap.getTileAt(playerInfo.Position + UP) == TileContent.House:
+            print('House at up')
+            return create_steal_action(UP)
+        if gameMap.getTileAt(playerInfo.Position + UP) == TileContent.Resource:
+            print('Ressouce at up')
+            return create_collect_action(UP)
+        return create_move_action(UP)
